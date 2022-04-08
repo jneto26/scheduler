a596ec8fcaafaaadf236ab73ad3b36f2532840b9
jneto26 <jeremyneto2023@u.northwestern.edu>
2022-04-07 20:43:51 -0500

    read from firebase

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React, { useState, useEffect } from 'react';
 import { hasConflict, getCourseTerm, getCourseNumber, addScheduleTimes, timeParts, terms} from './utilities/times.js';
 import CourseList from './components/CourseList.js';
+import { useData } from './utilities/firebase.js';
 
 const Banner = ({ title }) => (
   <h1>{ title }</h1>
@@ -9,20 +10,10 @@ const Banner = ({ title }) => (
 
 
 const App = () => {
-  const [schedule, setSchedule] = useState();
-  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
-
-  useEffect(() => {
-    const fetchSchedule = async () => {
-      const response = await fetch(url);
-      if (!response.ok) throw response;
-      const json = await response.json();
-      setSchedule(addScheduleTimes(json));
-    }
-    fetchSchedule();
-  }, []);
-
-  if (!schedule) return <h1>Loading schedule...</h1>;
+  const [schedule, loading, error] = useData('/', addScheduleTimes); 
+  
+  if (error) return <h1>{error}</h1>;
+  if (loading) return <h1>Loading the schedule...</h1>
 
   return (
     <div className="container">
@@ -31,6 +22,7 @@ const App = () => {
     </div>
   );
 };
+
 
 
 export default App;
---
+++ src/utilities/firebase.js
@@ -0,0 +1,43 @@
+import { initializeApp } from 'firebase/app';
+import { getDatabase, onValue, ref, set } from 'firebase/database';
+import { useState, useEffect } from 'react';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBGheV35ThQXYaB74sPDkyzUIcQRV1SKho",
+    authDomain: "scheduler-c04f6.firebaseapp.com",
+    databaseURL: "https://scheduler-c04f6-default-rtdb.firebaseio.com",
+    projectId: "scheduler-c04f6",
+    storageBucket: "scheduler-c04f6.appspot.com",
+    messagingSenderId: "1016027802018",
+    appId: "1:1016027802018:web:67fb27ccf4fbc4bc1faafb",
+    measurementId: "G-R73L6X7LZV"
+  };
+
+const firebase = initializeApp(firebaseConfig);
+const database = getDatabase(firebase);
+
+
+export const useData = (path, transform) => {
+    const [data, setData] = useState();
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState();
+  
+    useEffect(() => {
+      const dbRef = ref(database, path);
+      const devMode = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
+      if (devMode) { console.log(`loading ${path}`); }
+      return onValue(dbRef, (snapshot) => {
+        const val = snapshot.val();
+        if (devMode) { console.log(val); }
+        setData(transform ? transform(val) : val);
+        setLoading(false);
+        setError(null);
+      }, (error) => {
+        setData(null);
+        setLoading(false);
+        setError(error);
+      });
+    }, [path, transform]);
+  
+    return [data, loading, error];
+  };
